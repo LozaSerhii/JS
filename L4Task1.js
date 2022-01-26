0174ac17c91be850b15c49ea81e9e6071c65e2ed
LozaSerhii <88531518+LozaSerhii@users.noreply.github.com>
2022-01-26 21:08:31 +0200

    Add files via upload

---
+++ L4Task1.js
@@ -0,0 +1,26 @@
+function forEach(arr, callback) {
+	if(Array.isArray(arr) === true &&  )
+    {
+        if(typeof callback=== 'function')
+        {
+            for(let i=0;i<arr.length; i++)
+            {
+                 callback(arr[i],i, arr)
+            }
+        }
+        else
+        {
+            throw new Error('Error: second parameter type should be a function');
+        }
+    }
+    else
+    {
+        throw new Error('Error: first parameter type should be an array');
+    }
+}
+
+const arr = [1,2,3];
+
+forEach(arr, function (item, i, arr) { 
+     console.log(item, i, arr);
+})
